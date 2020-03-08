7e8ce0fb95bbf6eff7b1dc7ee3b75c54e2b77d38
sunny00217wm <59136112+sunny00217wm@users.noreply.github.com>
2020-03-08 21:39:12 +0800

    Update beta.zhwiki.workers.dev.js

--- beta.zhwiki.workers.dev.js
+++ beta.zhwiki.workers.dev.js
@@ -1,5 +1,8 @@
+var replyCallback
+
 addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
+  replyCallback = event.respondWith
+  handleRequest(event.request)
 })
 
 async function handleRequest(request) {
@@ -7,7 +10,7 @@ async function handleRequest(request) {
     u = (new URL(request.url))
     if(u.protocol == 'http:'){
       u.protocol = 'https:'
-      return (new Response('', {status: 301, headers: {'Location': u.href}}))
+      replyCallback('', {status: 301, headers: {'Location': u.href}})
     }
 
     h = (new Headers(request.headers))
@@ -20,28 +23,23 @@ async function handleRequest(request) {
 
     argv = u.search
 
-    if(path == '/'){ return (new Response('', {status: 302, headers: {'Location': '/wiki/'}}))}
-    //if is /w/api.php then return to 'https://zh.wikipedia.beta.wmflabs.org/w/api.php'
-    if(path == '/w/api.php'){ return (new Response('', {status: 302, headers: {'Location': 'https://zh.wikipedia.beta.wmflabs.org/w/api.php'}}))}
+    if(path == '/'){ replyCallback('', {status: 302, headers: {'Location': '/wiki/'}})}
+
+    if(path == '/w/api.php'){ replyCallback('', {status: 302, headers: {'Location': 'https://zh.wikipedia.beta.wmflabs.org/w/api.php'}})}
 
     url = path + argv
 
-    return fetch('https://zh.wikipedia.beta.wmflabs.org' + url, {
-      method : 'POST',
-      redirect: 'follow',
+    replyCallback('https://zh.wikipedia.beta.wmflabs.org' + url, {
+      method : request.method,
+      body: request.body
     }).then(function(d){
-      //var text = d.body
-      //new Response(text.replace(/\/\/zh\.wikipedia\.org/, 'www.zhwiki.workers.dev')
-      //.replace(/\/\/zh\.wikipedia\.beta\.wmflabs\.org/, 'beta.zhwiki.workers.dev'),
-      //  d.body
-      //)
-      //if d.url and 'https://zh.wikipedia.beta.wmflabs.org' + url isn't same then return to d.url
+
       if (d.url !== 'https://zh.wikipedia.beta.wmflabs.org' + url){
-        return (new Response('', {status: 302, headers: {'Location': d.url}}))
+        replyCallback('', {status: 302, headers: {'Location': d.url}})
       }
-      return (new Response(d.body, {status: d.status, headers: d.headers}))
+      replyCallback(d.body, {status: d.status, headers: d.headers})
     })
   } catch(e){
-    return (new Response(e, {status: 500}))
+    replyCallback(e, {status: 500})
   }
 }
